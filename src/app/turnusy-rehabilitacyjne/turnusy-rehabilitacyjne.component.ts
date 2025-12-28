7c0373926330f63831db93392fbddca5d573504a
Maksymilian Wiesiolek <mwiesiolek@carfax.eu>
2025-12-28 15:09:35 +0100

    update prices and vulnerabilities

--- src/app/turnusy-rehabilitacyjne/turnusy-rehabilitacyjne.component.ts
+++ src/app/turnusy-rehabilitacyjne/turnusy-rehabilitacyjne.component.ts
@@ -37,91 +37,83 @@ export class TurnusyRehabilitacyjneComponent {
   batchPrices: BatchPrice[] = [
     {
       id: 'Turnus I',
-      startDate: '4 maj 2025',
-      endDate: '17 maj 2025',
-      pfronParticipant: '2390zł',
-      pfronCaregiver: '2290zł',
-      private: '2290zł'
-    },
-    {
-      id: 'Turnus II',
-      startDate: '18 maj 2025',
-      endDate: '31 maj 2025',
-      pfronParticipant: '2500zł*',
-      pfronCaregiver: '2390zł',
-      private: '2390zł'
-    },
-    {
-      id: 'Turnus III',
-      startDate: '1 czerwiec 2025',
-      endDate: '14 czerwiec 2025',
-      pfronParticipant: '2500zł*',
-      pfronCaregiver: '2390zł',
-      private: '2390zł'
-    },
-    {
-      id: 'Turnus IV',
-      startDate: '15 czerwiec 2025',
-      endDate: '28 czerwiec 2025',
-      pfronParticipant: '2500zł*',
-      pfronCaregiver: '2390zł',
-      private: '2390zł'
-    },
-    {
-      id: 'Turnus V',
-      startDate: '29 czerwiec 2025',
-      endDate: '12 lipiec 2025',
-      pfronParticipant: '2700zł',
-      pfronCaregiver: '2500zł',
-      private: '2500zł'
-    },
-    {
-      id: 'Turnus VI',
-      startDate: '13 lipiec 2025',
-      endDate: '26 lipiec 2025',
-      pfronParticipant: '2700zł',
-      pfronCaregiver: '2500zł',
-      private: '2500zł'
-    },
-    {
-      id: 'Turnus VII',
-      startDate: '27 lipiec 2025',
-      endDate: '9 sierpień 2025',
-      pfronParticipant: '2700zł',
-      pfronCaregiver: '2500zł',
-      private: '2500zł'
-    },
-    {
-      id: 'Turnus VIII',
-      startDate: '10 sierpień 2025',
-      endDate: '23 sierpień 2025',
-      pfronParticipant: '2600zł',
+      startDate: '17 maj 2026',
+      endDate: '30 maj 2026',
+      pfronParticipant: '2600zł*',
       pfronCaregiver: '2400zł',
       private: '2400zł'
     },
     {
+      id: 'Turnus II',
+      startDate: '31 maj 2026',
+      endDate: '13 czerwiec 2026',
+      pfronParticipant: '2700zł*',
+      pfronCaregiver: '2500zł',
+      private: '2500zł'
+    },
+    {
+      id: 'Turnus III',
+      startDate: '14 czerwiec 2026',
+      endDate: '27 czerwiec 2026',
+      pfronParticipant: '2700zł*',
+      pfronCaregiver: '2500zł',
+      private: '2500zł'
+    },
+    {
+      id: 'Turnus IV',
+      startDate: '28 czerwiec 2026',
+      endDate: '11 lipiec 2026',
+      pfronParticipant: '2900zł',
+      pfronCaregiver: '2700zł',
+      private: '2650zł'
+    },
+    {
+      id: 'Turnus V',
+      startDate: '12 lipiec 2026',
+      endDate: '25 lipiec 2026',
+      pfronParticipant: '2900zł',
+      pfronCaregiver: '2700zł',
+      private: '2650zł'
+    },
+    {
+      id: 'Turnus VI',
+      startDate: '26 lipiec 2026',
+      endDate: '8 sierpień 2026',
+      pfronParticipant: '2900zł',
+      pfronCaregiver: '2700zł',
+      private: '2650zł'
+    },
+    {
+      id: 'Turnus VII',
+      startDate: '9 sierpień 2026',
+      endDate: '22 sierpień 2026',
+      pfronParticipant: '2900zł',
+      pfronCaregiver: '2700zł',
+      private: '2650zł'
+    },
+    {
+      id: 'Turnus VIII',
+      startDate: '23 sierpień 2026',
+      endDate: '5 wrzesień 2026',
+      pfronParticipant: '2800zł',
+      pfronCaregiver: '2500zł',
+      private: '2500zł'
+    },
+    {
       id: 'Turnus IX',
-      startDate: '24 sierpień 2025',
-      endDate: '6 wrzesień 2025',
-      pfronParticipant: '2600zł',
-      pfronCaregiver: '2390zł',
-      private: '2390zł'
+      startDate: '6 wrzesień 2026',
+      endDate: '19 wrzesień 2026',
+      pfronParticipant: '2800zł',
+      pfronCaregiver: '2500zł',
+      private: '2500zł'
     },
     {
       id: 'Turnus X',
-      startDate: '7 wrzesień 2025',
-      endDate: '20 wrzesień 2025',
-      pfronParticipant: '2600zł',
-      pfronCaregiver: '2390zł',
-      private: '2390zł'
-    },
-    {
-      id: 'Turnus XI',
-      startDate: '21 wrzesień 2025',
-      endDate: '4 październik 2025',
-      pfronParticipant: '2500zł*',
-      pfronCaregiver: '2390zł',
-      private: '2390zł'
+      startDate: '20 wrzesień 2026',
+      endDate: '3 październik 2026',
+      pfronParticipant: '2700zł*',
+      pfronCaregiver: '2500zł',
+      private: '2500zł'
     }
   ];
 
